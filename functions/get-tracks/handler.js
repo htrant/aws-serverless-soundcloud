3cbf60a17fdca981c38173e22cb361f44edd90c9
willydekido <trunghieu.tran138@gmail.com>
2016-08-03 16:02:34 +0300

    moved track request

--- functions/get-tracks/handler.js
+++ functions/get-tracks/handler.js
@@ -1,18 +1,37 @@
 'use strict';
 
-const apiEndpoint = process.env.SC_API_ENDPOINT;
+const rp = require('request-promise');
+const scApi = process.env.SC_API_ENDPOINT;
+const scClientId = process.env.SC_CLIENT_ID;
 
 
 module.exports.handler = (event, context, cb) => {
-  const tracks = [];
-  for (const item of event) {
-    const track = {
-      track_id: item.id,
-      title: item.title,
-      permalink_url: item.permalink_url,
-      artwork_url: item.artwork_url
-    };
-    tracks.push(track);
-  }
-  cb(null, tracks);
+  const options = {
+    uri: `${scApi}/users/${event.user_id}/tracks`,
+    qs: {
+      client_id: scClientId
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  };
+
+  rp(options)
+      .then(data => {
+        const tracks = [];
+        for (const item of data) {
+          const track = {
+            track_id: item.id,
+            title: item.title,
+            permalink_url: item.permalink_url,
+            artwork_url: item.artwork_url
+          };
+          tracks.push(track);
+        }
+        cb(null, tracks);
+      })
+      .catch(exception => {
+        cb(exception);
+      });
 };
